0bf95b233e6f455fcb6131bf0a9f35a34e96b525
hiendbui <hiendbui0@gmail.com>
2020-11-02 15:54:16 -0800

    Add users and sessions controllers

---
+++ cats_auth/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :already_logged_in, only: [:new,:create]
+
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      login_user!
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
+end
+
+
